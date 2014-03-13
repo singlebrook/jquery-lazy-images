bff40d137396d55fb27e49ea59824ea47ded9121
Tyler Nieman <tyler@tsnieman.net>
2014-03-12 23:34:58 -0700

    updating test spec for efee758

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -14,7 +14,7 @@ describe 'basic lazy image loading', :js => true do
 
     page.evaluate_script("scrollTo(0, 1000000)");
     page.should have_css 'img[src*="placekitten"]'
-    page.should_not have_css 'img[src*="grey.gif"]'
+    page.should_not have_css 'img[src*="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"]'
   end
 
   it "should load multiple images" do
@@ -31,10 +31,10 @@ describe 'basic lazy image loading', :js => true do
     it "should not display placeholder images" do
       visit basic_pages_path
       # I'd like to check the visibility, but RackTest doesn't even parse CSS...
-      # page.find('img[src*="grey.gif"]').should_not be_visible
+      # page.find('img[src*="DataUriGoesHere"]').should_not be_visible
       # So instead I'll just check that it has the "lazy" class, which is hidden
       # via CSS.
-      page.should have_css 'img.lazy[src*="grey.gif"]'
+      page.should have_css 'img.lazy[src*="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"]'
     end
   end
 end
